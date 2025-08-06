03eb03a8bf81e9a709a4f17eb3f508d6eae5437e
ragul-wenxt <ragul.arulkumaran@wenxttech.com>
2025-08-06 13:04:37 +0530

    initial

--- src/app/common/routerPathVariable.ts
+++
@@ -1,1 +0,0 @@
-export const DASHBOARD_PATH = '/dashboard/ri-basis'
--- src/environment/environment .ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://192.168.1.198:8080',  
-    // apiUrl: 'http://192.168.1.62:8085',  
-
-  };
-  
